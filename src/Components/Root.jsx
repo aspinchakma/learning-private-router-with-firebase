a763dc2032fedcb050d6125235866c01af78ab0f
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-09 22:14:06 +0600

    outlet setup

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 
 const Root = () => {
   return (
-    <div>
+    <div className="w-[80%] mx-auto flex flex-col min-h-screen">
       <Header />
+      <div className="flex-1">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
