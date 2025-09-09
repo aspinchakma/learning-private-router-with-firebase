e177f573db46121f4d88d110b44a285a2cd3e314
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-09 22:10:15 +0600

    route setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home.jsx";
 import Root from "./Components/Root.jsx";
+import SignIn from "./Components/SignIn.jsx";
+import SignUp from "./Components/SignUp.jsx";
 import "./index.css";
 const router = createBrowserRouter([
   {
-    path: "",
+    path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
