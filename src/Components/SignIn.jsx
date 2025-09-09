271b5d90b9ab1796153c6c7adf70389d974e62f2
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-09 23:36:38 +0600

    setup login form

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,7 +1,23 @@
 const SignIn = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>This is Sign In</h2>
+    <div className="hero min-h-screen flex items-center justify-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="card-body">
+          <form onSubmit={handleLogin} className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
