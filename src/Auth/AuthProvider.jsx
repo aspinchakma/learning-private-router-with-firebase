2c228bac372f7f853d71eb79618a6d04af0bb025
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-10 14:19:23 +0600

    subscribed setup on auth provider

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,13 +1,21 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../firebase.init";
 import { AuthContext } from "./Context";
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({});
-  onAuthStateChanged(auth, (userInfo) => {
-    setUser(userInfo);
-  });
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const unSubscribed = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("current user", currentUser);
+      console.log("user", user);
+    });
+    return () => {
+      unSubscribed();
+    };
+  }, []);
+
   console.log(user);
   return (
     <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
